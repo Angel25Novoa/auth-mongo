c0d41a1dd470fa0be4ca3cc43d77d2ff88ae2e2f
Angel25Novoa <jorge.250495@gmail.com>
2022-07-30 12:22:37 -0500

    cors changed

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const cors = require('cors')
 const usersRouter = require('./users')
 const registerRouter = require('./register')
 const authRouter = require('./auth')
 const authMiddleware = require('../middlewares/authorization')
-
-router.use(cors())
 
 router.use('/auth', authRouter)
 router.use('/register', registerRouter)
