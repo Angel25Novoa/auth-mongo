762b96427dd461972c53a5f344cf79ae7b5892fd
Angel25Novoa <jorge.250495@gmail.com>
2022-07-30 11:50:41 -0500

    npm start added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('./src/app')
 require('dotenv').config()
 require('./src/db/mongo')
 
-const port = process.env.PORT || 5001
+const port = process.env.PORT
 
 app.listen(port, () => {
   console.log(`El servidor esta corriendo en el puerto ${port}`)
