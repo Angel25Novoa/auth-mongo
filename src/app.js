a836415d2e99fdf1c377de8162c47245fad3c04d
Angel25Novoa <jorge.250495@gmail.com>
2022-07-30 11:57:47 -0500

    message updated

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ app.use(express.json())
 app.use('/api/v1', apiRouter)
 
 app.use('/', (req, res) => {
-  res.send({message: 'Si ves esto en una ruta que'})
+  res.send({message: 'Si ves esto en una ruta que no sea la raíz la regaste'})
 })
 
 
